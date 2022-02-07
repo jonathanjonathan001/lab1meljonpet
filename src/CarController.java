5c853df6e40273a55f7fa06df4182d4f31f80591
jonathanjonathan001 <jonathanrosemark@gmail.com>
2022-02-07 17:03:36 +0100

    Changes by melindastromberg

--- src/CarController.java
+++ src/CarController.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 /*
--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -48,7 +48,6 @@ public class DrawPanel extends JPanel{
         {
             ex.printStackTrace();
         }
-
     }
 
     // This method is called each time the panel updates/refreshes/repaints itself
