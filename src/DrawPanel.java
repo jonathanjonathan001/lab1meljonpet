f1d7fd2c077c8fbaa57a37b953934cfceb9e18cc
petter <petter.reingardt@gmail.com>
2022-02-16 17:57:41 +0100

    Changes by melindastromberg

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 
 // This panel represents the animated part of the view with the car images.
 
-public class DrawPanel extends JPanel{
+public class DrawPanel extends JComponent{
 
     // Just a single image, TODO: Generalize
     BufferedImage volvoImage;
